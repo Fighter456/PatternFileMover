c7b8d1149ae0379f95ec34edb372e2a8b3b932d0
Dennis Kraffczyk <dennis.kraffczyk@gmail.com>
2022-12-04 15:44:19 +0100

    Show `FolderBrowserDialog` on double click in cell

--- PatternFileMover/NameAssociationsForm.Designer.cs
+++ PatternFileMover/NameAssociationsForm.Designer.cs
@@ -41,6 +41,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(545, 161);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // NameAssociationsForm
             // 
--- PatternFileMover/NameAssociationsForm.cs
+++ PatternFileMover/NameAssociationsForm.cs
@@ -14,13 +14,28 @@ namespace PatternFileMover
         private void NameAssociationsForm_Load(object sender, EventArgs e)
         {
             dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
-            dataGridView1.RowHeadersVisible = false;
 
             var dataList = new BindingList<NameAssociationsData>(NameAssociations.LoadFromExistingConfigFile());
             var dataSource = new BindingSource(dataList, null);
             dataGridView1.DataSource = dataSource;
 
             dataGridView1.AutoResizeColumns();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // equals the column for the target directory
+            if (e.ColumnIndex == 1)
+            {
+                var folderBrowserDialog = new FolderBrowserDialog();
+                folderBrowserDialog.SelectedPath = dataGridView1.SelectedCells[0].Value.ToString();
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    dataGridView1.SelectedCells[0].Value = folderBrowserDialog.SelectedPath;
+                }
+            }
         }
     }
 }
